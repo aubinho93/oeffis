2b1a7c03d36c0c7b080de558129d300767c0677d
green-carrot@orange-cactus.org <green-carrot@orange-cactus.org@8d566e10-d6d2-11de-9f34-657d07d5c07a>
2009-11-22 12:57:09 +0000

    enable settings for all activities

--- trunk/oeffis/src/net/oeffis/gui/StationSearch.java
+++ trunk/oeffis/src/net/oeffis/gui/StationSearch.java
@@ -6,14 +6,9 @@ import net.oeffis.Preferences;
 import net.oeffis.R;
 import net.oeffis.data.DataClient;
 import net.oeffis.data.Departure;
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,19 +16,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
-public class StationSearch extends Activity {
+public class StationSearch extends ActivityBase {
 
 	private DataClient<String, Departure> dataClient;
 	private Preferences preferences;
 	private ArrayList<String> stations;
 	private ArrayAdapter<String> stationAdapter;
-	
-	private Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			stationAdapter.notifyDataSetChanged();
-		};
-	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +30,7 @@ public class StationSearch extends Activity {
 		
 		stations = new ArrayList<String>();
 		stationAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stations);
-		
 		preferences = new Preferences(this);
-		dataClient = (DataClient<String, Departure>) preferences.getDataClient();
 		
 		ListView stations = (ListView) findViewById(R.id.stations);
 		stations.setAdapter(stationAdapter);
@@ -54,38 +40,24 @@ public class StationSearch extends Activity {
 				startActivity(intent);
 			}
 		});
-		
-		reloadList();
 	}
 	
 	private void reloadList() {
+		
+		if(dataClient != preferences.getDataClient()) {
+			dataClient = (DataClient<String, Departure>) preferences.getDataClient();
+		}
+		
 		stations.clear();
 		stations.addAll(dataClient.getStations());
-		handler.sendEmptyMessage(0);
+		stationAdapter.notifyDataSetChanged();
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		preferences = new Preferences(this);
 		if(dataClient != preferences.getDataClient()) {
 			reloadList();
 		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()) {
-		case R.id.menu_settings:
-			startActivity(new Intent(this, Settings.class));
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
