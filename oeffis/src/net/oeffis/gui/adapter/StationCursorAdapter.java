1d21c0674b2e76673bc4154bb9463a4e35ac237a
green-carrot <green-carrot@orange-cactus.org>
2010-04-24 15:07:25 +0000

    fixed favorite selection bug

--- oeffis/src/net/oeffis/gui/adapter/StationCursorAdapter.java
+++ oeffis/src/net/oeffis/gui/adapter/StationCursorAdapter.java
@@ -6,33 +6,50 @@ import net.oeffis.data.db.DbAdapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.SimpleCursorAdapter;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class StationCursorAdapter extends SimpleCursorAdapter {
+public class StationCursorAdapter extends SimpleCursorAdapter implements OnClickListener {
 	
 	private DbAdapter db;
+	private Cursor cursor;
 	
 	public StationCursorAdapter(Context context, DbAdapter db, Cursor c) {
 		super(context, R.layout.station_listitem, c,
-			new String[] { "station" }, new int[] { R.id.station });
+			new String[] { "station", "favorite" }, new int[] { R.id.station, R.id.favorite });
+		
 		this.db = db;
+		this.cursor = c;
+		
+		this.setViewBinder(new ViewBinder() {
+			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+				if(columnIndex == 2) {
+					CheckBox cb = (CheckBox) view;
+					cb.setChecked(cursor.getInt(2) > 0);
+					return true;
+				}	
+				return false;
+			}
+		});	
 	}
 	
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
 		super.bindView(view, context, cursor);
 		
-		final Station station = DbAdapter.fromCursor(cursor);
-		final CheckBox favorite = (CheckBox) view.findViewById(R.id.favorite);
-		favorite.setChecked(station.isFavorite());
-		favorite.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				db.setFavorite(station.getId(), favorite.isChecked());
-			}
-		});
+		Station station = DbAdapter.fromCursor(cursor);
 		view.setTag(station);
+		
+		CheckBox cb = (CheckBox) view.findViewById(R.id.favorite);
+		cb.setTag(station);
+		cb.setOnClickListener(this);
+	}
+	
+	public void onClick(View v) {
+		CheckBox checkbox = (CheckBox) v;
+		Station s = (Station) v.getTag();
+		db.setFavorite(s.getId(), checkbox.isChecked());
+		cursor.requery();
 	}
 }
